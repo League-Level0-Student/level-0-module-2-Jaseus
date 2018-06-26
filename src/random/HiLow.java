20b30d6c737c73619c22d9ecaccb669bb7326f8b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-26 14:28:13 -0700

    HiLow Game - DiceRoll - IntroToRandom - RockPaperScissors - Validation

---
+++ src/random/HiLow.java
@@ -0,0 +1,28 @@
+package random;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+// *THIS IS INCOMPLETE*
+public class HiLow {
+public static void main(String[] args) {
+	Random randomMaker = new Random();
+	int randomNumber = randomMaker.nextInt(101);
+	System.out.println(randomNumber);
+	
+	String number = JOptionPane.showInputDialog("Guess an integer from 0-100");
+		int num = Integer.parseInt(number);
+			
+			if(num == randomNumber) {
+				JOptionPane.showMessageDialog(null, "Correct! The number was " + randomNumber);
+			}
+				if(num < randomNumber) {
+					JOptionPane.showInputDialog("Incorrect. Guess a higher number");
+				}
+				if(num > randomNumber) {
+					JOptionPane.showInputDialog("Incorrect. Guess a lower number");
+				}
+			}
+			
+}
+
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -12,16 +12,32 @@ public class Validation {
 
 		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://vimeo.com/2485018
 		
-		Random randomMaker = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
 		
 		// 2. On paper, write all the numbers that get printed when you run this class 10 times
 		
-		System.out.println(randomNumber);
+	
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
-
+		for (int i = 0; i < 10; i++) {
+		
+			Random randomMaker = new Random();
+			int randomNumber = randomMaker.nextInt(5);
+			
+			System.out.println(randomNumber);
+			
+			if(randomNumber == 0) {
+				JOptionPane.showMessageDialog(null, "You're looking pretty swell today :D");
+			} else if(randomNumber == 1) {
+				JOptionPane.showMessageDialog(null, "You're precious");
+			} else if(randomNumber == 2) {
+				JOptionPane.showMessageDialog(null, "You're heart is bigger than most others :)");
+			} else if(randomNumber == 3) {
+				JOptionPane.showMessageDialog(null, "You're personality is very enjoyable");
+			} else if(randomNumber == 4) {
+				JOptionPane.showMessageDialog(null, "You're the reaason we smile :)");
+			}
+		}
 		// 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
